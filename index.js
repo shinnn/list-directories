d35a1b6edb7f66662193340feefe52e2298522ac
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-08-03 19:40:56 +0900

    remove fallback for environments with no native Map suport
    [BREAKING] drop support for Node.js < 0.12

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var ES6Set = require('es6-set');
 var lstatDir = require('lstat-dir');
 var toArray = require('lodash/fp/toArray');
 
 function filterDirectories(map) {
-  var directoryPaths = new ES6Set();
+  var directoryPaths = new Set();
 
   toArray(map).forEach(function(pathStatPair) {
     if (pathStatPair[1].isDirectory()) {
